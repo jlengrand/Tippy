c2ca68dd4978b8e3faa58dbf38fd7320ebb5d029
Julien Lengrand-Lambert <julien@lengrandlambert.fr>
2011-12-30 14:16:04 +0100

    First correct packaging
    Package is uploaded to Pypi with correct values.
    setup files is filled with important data.
    package can be loaded into ipython and used.
    examples run but images still can't be found. 
    TODO:
    	Access example images from within folder.
    	copy readme and doc into package too. 
    	crezate correct html documentation
    
    Reported-by: Julien Lengrand-Lambert
    Signed-off-by: Julien Lengrand-Lambert <julien@lengrandlambert.fr>

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+'''
+Tippy : another Toolbox for Image Processing in PYthon
+
+Created on Nov 19, 2011
+Copyright (C) 2011  Julien Lengrand-Lambert <julien@lengrand.fr>
+
+@author: Julien Lengrand-Lambert
+@email: julien@lengrand.fr
+'''
+try:
+    import cv
+except:
+    print ''
+    print 'Tippy requires the latest OpenCV Python bindings'
+    print 'See: http://opencv.willowgarage.com/wiki// for OpenCV installation'
+    print 'and http://opencv.willowgarage.com/wiki/PythonInterface for Python bindings'
+    sys.exit(0)
+
 from distutils.core import setup
 
 setup(
@@ -5,12 +23,24 @@ setup(
     version='0.1',
     author='Julien Lengrand-Lambert',
     author_email='julien@lengrand.fr',
-    packages=['tippy', 'tippy.tests', 'examples'],
+    packages=['tippy', 'tippy.tests', 'tippy.examples'],
+    package_data={'tippy': ['data/*.*']},
     url='https://github.com/jlengrand/Tippy',
     license='LICENSE.txt',
     description='another Toolbox for Image Processing in PYthon, based on OpenCV',
     long_description=open('README.txt').read(),
-    install_requires=[
-        "OpenCV >= 3.1",
-    ],
+    classifiers = ['Development Status :: 2 - Pre-Alpha',
+                   'Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Environment :: Console',
+                   'Environment :: Plugins',
+                   'Operating System :: OS Independent',
+                   'Operating System :: Unix',
+                   'Programming Language :: Python ',
+                   'Programming Language :: C ',
+                   'Natural Language :: English ',
+                   'Topic :: Utilities',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Software Development'],
 )
